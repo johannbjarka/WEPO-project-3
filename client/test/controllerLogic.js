1f36f0056f9e9ea849cb94d89d9943dd8141ac6f
Carl Andreas Sveinsson <carl13@ru.is>
2015-03-02 17:37:53 +0000

    Bower and tests

---
+++ client/test/controllerLogic.js
@@ -0,0 +1,28 @@
+describe( "HomeCtrl login", function( ){
+	var ctrl, scope;
+	var fakeChat = {
+		logonUser: function(username){
+		return username == "dabs";},
+		getRoomList: function(){ return ["lobby", "smu"];}
+	};
+	beforeEach( module("MyApp") );
+
+	beforeEach(inject(function ($controller, $rootScope) {
+		$scope = $rootScope.$new();
+		ctrl = $controller("HomeCtrl", {
+		$scope: $scope,
+		ChatBackend: fakeChat });
+	}));
+
+	it("should request a list of rooms if username
+		is available", function(){
+		// Arrange:
+		var user = "dabs";
+		scope.username = user;
+		// Act:
+		ctrl.doLogin();
+		// Assert:
+		expect(fakeChat.getRoomList)
+		.toHaveBeenCalledWith(user);
+	});
+});
