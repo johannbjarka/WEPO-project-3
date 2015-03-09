ce5ed566c3ebd1ea7cbb593b194aca6f188972c2
johannbjarka <johannbjarka@gmail.com>
2015-03-09 14:46:39 +0000

    Minor fixes

--- client/js/LoginFactory.js
+++ client/js/LoginFactory.js
@@ -8,7 +8,7 @@ angular.module('Evaluator').factory('LoginFactory', [
 					username: user,
 					password: pass
 				}).success(function(response) {
-					$http.defaults.headers.common.Autherization = 'Basic' + response.data.token;
+					$http.defaults.headers.common.Authorization = 'Basic' + response.data.token;
 					return response.data;
 				}).error(function(response) {
 					return response.message;
--- client/js/app.js
+++ client/js/app.js
@@ -1,2 +1,2 @@
 var Evaluator = angular.module('Evaluator', ['ngRoute', 'ngAnimate', 'toastr']);
-angular.module("Evaluator").constant("API", "http://dispatch.hir.is/demo/doc/api/v1");
+angular.module("Evaluator").constant("API", "http://localhost:19358/api/v1");
