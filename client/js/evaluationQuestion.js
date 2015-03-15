41cebb9cb02545abc6bb495c54877168b6eadc70
Sigtryggur Ómarsson <kvurit@gmail.com>
2015-03-15 06:46:07 +0000

    Added layout for viewing created templates. Started working on evaluation Question directive, now displays questions but doesn't post answers correctly.

--- client/js/evaluationQuestion.js
+++ client/js/evaluationQuestion.js
@@ -1,15 +1,15 @@
 angular.module('Evaluator').directive('evaluationQuestion', function () {
 	return {
-		restrict: "E",
+		restrict: 'E',
 		replace: true,
 		templateUrl: 'views/evaluationQuestion.html',
-		//template: '<div><div>',
-		scope: { question: '=question'},
-		
-		link: function ( scope, element, attr ) {
-			scope.$watch('question', function(question) {
-			console.log(scope);
-			console.log(scope.question, question);
+		scope: {
+			ngQuestion: '=',
+			ngIndex: '@',
+			ngFunc: '&',
+		},
+		link: function (scope, element, attr) {
+			scope.$watch('answer', function(value) {
 			});
 		}
 	};
