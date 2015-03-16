3b3a9f3d72eb4b2fbf97d1de4cc8c96ba9d5b2e8
Sigtryggur Ómarsson <kvurit@gmail.com>
2015-03-16 05:15:15 +0000

    response token is now saved too localStorage. Token is stored for 30minutes.

--- client/js/LoginFactory.js
+++ client/js/LoginFactory.js
@@ -1,11 +1,14 @@
-angular.module('Evaluator').factory('LoginFactory', ['$http', 'API',
-	function($http, API) {
+angular.module('Evaluator').factory('LoginFactory', ['$http', '$window', 'API',
+	function($http, $window, API) {
 		return {
 			login: function(user, pass) {
 				return $http.post(API + '/login', {
 					user: user,
 					pass: pass
 				}).success(function(response) {
+					$window.localStorage['LastUpdated'] = new Date();
+					$window.localStorage['Token'] = response.Token;
+					$window.localStorage['User'] = response.User;
 					$http.defaults.headers.common.Authorization = 'Basic ' + response.Token;
 					return response;
 				}).error(function(response) {
--- client/js/app.js
+++ client/js/app.js
@@ -1,15 +1,33 @@
 var Evaluator = angular.module('Evaluator', ['ngRoute', 'ngAnimate', 'toastr', 'ui.bootstrap', 'chart.js']);
 angular.module("Evaluator").constant("API", "http://localhost:19358/api/v1");
 
+angular.module('Evaluator').run(['$http', '$window',
+	function ($http, $window) {
+		var storage = $window.localStorage;
+		if(storage['LastUpdated'] !== undefined) {
+			var cacheTime = 30 * 60000;
+			var time = new Date(new Date(storage['LastUpdated']).getTime() + cacheTime);
+			if(time > new Date()) {
+				$http.defaults.headers.common.Authorization = 'Basic ' + storage['Token'];
+				storage['LastUpdated'] = new Date();
+			}
+			else {
+				storage['Token'] = '';
+				storage['User'] = [];
+			}
+		}
+	}
+]);
+
 Evaluator.directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
-                event.preventDefault();
-            }
-        });
-    };
+	return function (scope, element, attrs) {
+		element.bind("keydown keypress", function (event) {
+			if(event.which === 13) {
+				scope.$apply(function (){
+					scope.$eval(attrs.ngEnter);
+				});
+				event.preventDefault();
+			}
+		});
+	};
 });
